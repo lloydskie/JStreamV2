704ca3c1a3f892da54a2221f7263a51e155b722d
John Lloyd Quijano <dsafjeioruy8439.fwsefj143@gmail.com>
2025-10-26 17:06:53 +0800

    a

---
+++ app/build/tmp/kapt3/stubs/release/com/jstreamv2/MainRepository.java
@@ -0,0 +1,14 @@
+package com.jstreamv2;
+
+@kotlin.Metadata(mv = {1, 9, 0}, k = 1, xi = 48, d1 = {"\u0000\u0012\n\u0002\u0018\u0002\n\u0002\u0010\u0000\n\u0002\b\u0002\n\u0002\u0010\u000e\n\u0000\u0018\u00002\u00020\u0001B\u0005\u00a2\u0006\u0002\u0010\u0002J\u0006\u0010\u0003\u001a\u00020\u0004\u00a8\u0006\u0005"}, d2 = {"Lcom/jstreamv2/MainRepository;", "", "()V", "getWelcomeMessage", "", "app_release"})
+public final class MainRepository {
+    
+    public MainRepository() {
+        super();
+    }
+    
+    @org.jetbrains.annotations.NotNull
+    public final java.lang.String getWelcomeMessage() {
+        return null;
+    }
+}
